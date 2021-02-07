2043c3991fe9f7dbf27e028454a9758275678a6e
Stefan Krstikj <st.krstic@gmail.com>
2021-02-07 12:44:24 +0100

    add routing links

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,11 +21,23 @@ const routes: Array<RouteConfig> = [
   {
     path: '/confirmshaming',
     name: 'Confirmshaming',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ConfirmShaming.vue')
-  }
+  },
+  {
+    path: '/pricecomparison',
+    name: 'Price Comparison Prevention',
+    component: () => import(/* webpackChunkName: "about" */ '../views/PriceComparison.vue')
+  },
+  {
+    path: '/sneakintobasket',
+    name: 'Sneak Into Basket and Roach Motel',
+    component: () => import(/* webpackChunkName: "about" */ '../views/SneakIntoBasketAndRoachMotel.vue')
+  },
+  {
+    path: '/hiddencosts',
+    name: 'Hidden Costs',
+    component: () => import(/* webpackChunkName: "about" */ '../views/HiddenCosts.vue')
+  },
 ]
 
 const router = new VueRouter({
