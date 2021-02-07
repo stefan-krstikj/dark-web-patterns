95439805bcf1ce2f01a368ad37ea13315f96bc9e
Stefan Krstikj <st.krstic@gmail.com>
2021-02-07 12:43:56 +0100

    add router links to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/trickquestion">Trick Question</router-link> |
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/trickquestion">Trick Question</router-link>
+      |
       <router-link to="/confirmshaming">Confirmshaming</router-link>
+      |
+      <router-link to="/pricecomparison">Price Comparison Prevention</router-link>
     </div>
     <router-view/>
   </div>
