7607987eb517018674213fcda10884ca9e1a914c
Stefan Krstikj <st.krstic@gmail.com>
2021-02-07 12:43:42 +0100

    add ConfirmShaming.vue

--- src/views/ConfirmShaming.vue
+++ src/views/ConfirmShaming.vue
@@ -48,3 +48,14 @@
 
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Navbar from "@/components/Navbar.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    Navbar
+  },
+})
+export default class ConfirmShaming extends Vue {}
+</script>
