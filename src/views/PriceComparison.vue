d4f9075e5ba35b01337751454274a9b1eff7cccf
Stefan Krstikj <st.krstic@gmail.com>
2021-02-13 18:26:57 +0100

    update PriceComparison.vue values

--- src/views/PriceComparison.vue
+++ src/views/PriceComparison.vue
@@ -4,11 +4,6 @@
     <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
 
       <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-        <div>
-          <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-            Brand new
-          </p>
-        </div>
         <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
         <span class="relative inline-block">
           <svg viewBox="0 0 52 24" fill="currentColor"
@@ -25,7 +20,7 @@
           нешто нешто нешто тука
         </h2>
         <p class="text-base text-gray-700 md:text-lg">
-          Тука некој смешен сабтајтл
+          Додадете некој албум во кошничка... Ајде...
         </p>
       </div>
       <div class="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
@@ -50,28 +45,28 @@ import ProductItem from "@/components/ProductItemComponent.vue";
 export default class PriceComparison extends Vue {
   productItems = [
     {
-      name: "Product 1",
-      description: "This is a description for product 1",
+      name: "OK Computer",
+      description: "OK Computer is the third studio album by English rock band Radiohead, released on 21 May 1997 on EMI subsidiaries Parlophone and Capitol Records. The members of Radiohead self-produced the album with Nigel Godrich, an arrangement they have used for their subsequent albums.",
       price: "199ден / парче",
       img: "radiohead.jpg"
     },
     {
-      "name": "Product 2",
-      "description": "This is a description for product 2",
+      "name": "Californication",
+      "description": "Californication is the seventh studio album by American rock band Red Hot Chili Peppers. It was released on June 8, 1999, on Warner Bros. Records and was produced by Rick Rubin",
       "price": "299ден / парче",
       img: "rhcp.jpg"
     },
     {
-      "name": "Product 3",
-      "description": "This is a description for product 3",
+      "name": "Po Tebe",
+      "description": "Po tebe (in Macedonian Cyrillic: По тебе) is the fifth studio album by the Macedonian singer Toše Proeski. The album was released in Macedonia and subsequently in Serbia, Montenegro, Bosnia and Herzegovina, Croatia and Slovenia under the Serbian title Pratim te",
       "price": "5ден / минута",
       img: "tose.jpg"
     },
     {
-      "name": "Product 4",
-      "description": "This is a description for product 4",
+      "name": "Sgt Pepper's Lonely Hearts Club Band",
+      "description": "Sgt. Pepper's Lonely Hearts Club Band is the eighth studio album by the English rock band the Beatles. Released on 26 May 1967, it spent 27 weeks at number one on the Record Retailer chart in the United Kingdom and 15 weeks at number one on the Billboard Top LPs chart in the United States.",
       "price": "9ден / песна",
-      img: "radiohead.jpg"
+      img: "beatles.jpg"
     }
   ]
 }
